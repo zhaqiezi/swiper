0e1b5ac4da3b072dbf4ec7c3b03a31ee0e94a700
hzw <434321624@qq.com>
2017-10-12 18:03:11 +0800

    改好又出错

--- js/banner.js
+++ js/banner.js
@@ -31,15 +31,21 @@ window.onload = function() {
         var pr = selector.substring(0, 1);
         var s = selector.substring(1);
         var arr = [];
+        var allE = null;
         if (pr == '#') {
             arr.push(document.getElementById(s))
         } else if (pr == '.') {
-            var allE = parent ? parent.getElementsByTagName('*') : document.getElementsByTagName('*');
+            allE = parent ? parent.getElementsByTagName('*') : document.getElementsByTagName('*');
             var reg = new RegExp("(^|\\s)" + s + "($|\\s)");
             for (var i = 0; i < allE.length; i++) {
                 if (reg.test(allE[i].className)) arr.push(allE[i]);
             }
             return arr;
+        } else if (pr == '>') {
+            allE = parent.getElementsByTagName(s);
+            for (var i = 0; i < allE.length; i++) {
+                if (allE[i].parentNode == parent) arr.push(allE[i]);
+            }
         } else {
             arr = parent ? parent.getElementsByTagName(selector) : document.getElementsByTagName(selector);
         }
@@ -113,8 +119,8 @@ window.onload = function() {
             this.timer = null; //轮播定时器
             this.cTimer = null; //动画效果定时器
             this.index = options.index + arr.length - 1; //轮播的索引
-            this.w = css(arr[0], 'width'); //宽度
-            this.h = css(arr[0], 'height'); //高度
+            this.w = css(el, 'width'); //宽度
+            this.h = css(el, 'height'); //高度
             this.status = 1; //定时器运行的状态
         }
 
@@ -128,13 +134,14 @@ window.onload = function() {
                 p.key = key;
                 p.from = css(el, key);
                 p.to = o[key];
-                p.n = (parseInt(p.to) - parseInt(p.from)) * 10 / t;
+                p.n = (parseInt(p.to) - parseInt(p.from)) * 30 / t;
                 arr.push(p);
             }
             for (var i = 0; i < arr.length; i++) {
                 var x = arr[i];
                 self.cTimer = setInterval(function() {
                     if (css(el, x.key) !== x.to) {
+                        console.log(x.to);
                         el.style[x.key] = parseInt(x.from) + x.n + dw;
                         x.from = css(el, x.key);
                     } else {
@@ -142,13 +149,12 @@ window.onload = function() {
                         self.cTimer = null;
                         f();
                     }
-                }, 10)
+                }, 30)
             }
         };
 
         // 结构生成
         Banner.prototype.structure = function() {
-            var self = this;
             var op = this.options;
             var el = this.el;
             var arr = this.arr.concat(this.arr).concat(this.arr);
@@ -157,12 +163,11 @@ window.onload = function() {
             var box = document.createElement('div');
             box.className = 'box';
             this.box = box;
-            var w = parseInt(css(arr[0], 'width')) * arr.length;
             for (var i = 0; i < arr.length; i++) {
                 box.appendChild(arr[i].cloneNode(true));
             }
 
-            // 加上前后箭头
+            // 箭头
             if (op.arrow) {
                 var arrowP = document.createElement('button');
                 var arrowN = document.createElement('button');
@@ -170,39 +175,33 @@ window.onload = function() {
                 arrowP.innerHTML = '上一个';
                 arrowN.className = 'next';
                 arrowN.innerHTML = '下一个';
-                self.pre = arrowP;
-                self.next = arrowN;
+                this.pre = arrowP;
+                this.next = arrowN;
                 x.appendChild(arrowP);
                 x.appendChild(arrowN);
             }
-            //加上导航 
+
+            //分页器
             if (op.dots) {
                 var dotList = document.createElement('ul');
                 dotList.className = 'dots-list';
                 var dots = '';
-                for (var i = 0; i < self.arr.length; i++) {
+                for (var i = 0; i < this.arr.length; i++) {
                     dots = dots + '<li class="dot-item">' + (i + 1) + '</li>';
                 }
                 dotList.innerHTML = dots;
                 this.dots = selectEl('.dot-item', dotList);
                 x.appendChild(dotList);
+                this.dotChange();
             }
+
             x.appendChild(box)
             el.innerHTML = '';
             el.appendChild(x);
-            css(x, {
-                'width': self.w,
-                'height': self.h,
-                'overflow': 'hidden'
-            })
-            if (op.way == 'move' && !op.vertical) {
-                css(box, {
-                    'width': w + 'px',
-                    'left': -self.index * parseInt(self.w) + 'px'
-                });
-                self.dotChange();
+            var allItem = selectEl('>*', box);
+            for (var i = 0; i < allItem.length; i++) {
+                css(allItem[i], { 'width': this.w, 'height': this.h });
             }
-
             return this;
         };
 
@@ -210,26 +209,22 @@ window.onload = function() {
         Banner.prototype.bindEvent = function() {
             var self = this;
             var ops = self.options;
-            this.el.onmouseenter = function() {
-                clearInterval(self.timer);
-                self.timer = null;
-            };
-            this.el.onmouseleave = function() {
-                self.offset();
-            };
+            if (ops.autoPlay) {
+                this.el.onmouseenter = function() {
+                    clearInterval(self.timer);
+                    self.timer = null;
+                };
+                this.el.onmouseleave = function() {
+                    self.setTimer();
+                };
+            }
             if (ops.dots) {
                 for (var i = 0; i < this.dots.length; i++) {
                     this.dots[i].index = i + this.arr.length;
                     addHandler(this.dots[i], self.options.event, function() {
+                        self.index = this.index;
                         if (self.status) {
-                            self.index = this.index;
-                            self.status = 0;
-                            self.dotChange();
-                            self.animation(self.box, {
-                                'left': -self.index * parseInt(self.w) + 'px'
-                            }, self.options.tTime, function() {
-                                self.status = 1;
-                            }, 'px');
+                            self.offset();
                         }
                     })
                 }
@@ -238,13 +233,13 @@ window.onload = function() {
                 addHandler(self.pre, self.options.event, function() {
                     if (self.status) {
                         self.index--;
-                        self.levelMove();
+                        self.offset();
                     }
                 });
                 addHandler(this.next, self.options.event, function() {
                     if (self.status) {
                         self.index++;
-                        self.levelMove()
+                        self.offset();
                     }
                 });
             }
@@ -262,70 +257,55 @@ window.onload = function() {
             }
         }
 
-        // 配置
+        // 配置效果
         Banner.prototype.offset = function() {
-            var self = this;
-            var op = this.options;
-            if (op.way === 'fade') {
-                return this;
-            } else if (!op.vertical) {
-                this.setTimer(function() {
-                    if (self.status) {
-                        self.index++;
-                        self.levelMove(self);
-                    }
-                });
-                return this;
-            }
+            throw new Error('必须设置切换效果')
         };
 
         // 判断迭代是否重置
         Banner.prototype.judge = function() {
-            if (this.index >= this.arr.length * 2) {
-                this.index = this.arr.length;
-                css(this.box, 'left', -(this.index - 1) * parseInt(this.w) + 'px');
-            } else if (this.index <= this.arr.length - 1) {
-                this.index = this.arr.length * 2 - 1;
-                css(this.box, 'left', -(this.index + 1) * parseInt(this.w) + 'px');
-            }
-        };
-
-        // 水平移动的效果 
-        Banner.prototype.levelMove = function() {
-            var self = this;
-            self.status = 0;
-            self.judge()
-            self.dotChange();
-            self.animation(self.box, {
-                'left': -self.index * parseInt(self.w) + 'px'
-            }, self.options.tTime, function() {
-                self.status = 1;
-            }, 'px');
+            throw new Error('必须设置判断边界的函数');
         };
 
         // 设置定时器
-        Banner.prototype.setTimer = function(f) {
-            // var box = this.box;
+        Banner.prototype.setTimer = function() {
             var self = this;
-            this.timer = setInterval(f, self.options.wTime + self.options.tTime);
+            this.timer = setInterval(function() {
+                if (self.status) {
+                    self.index++;
+                    self.offset();
+                }
+            }, self.options.wTime + self.options.tTime);
+            return this;
         };
 
-        // 水平方向的构造函数
+        // 水平方向平移
         function LevelMove(el, arr, op) {
             Banner.call(this, el, arr, op);
         }
 
         extend(Banner, LevelMove);
 
+        // 设置单独样式
+        LevelMove.prototype.setStyles = function() {
+            css(this.box, {
+                'height': '100%',
+                'width': parseInt(this.w) * this.arr.length * 3 + 'px',
+                'left': -this.index * parseInt(this.w) + 'px'
+            });
+            return this;
+        }
+
         // 水平平移方向的动画效果
         LevelMove.prototype.offset = function() {
-            this.status;
-            this.judge()
+            var self = this;
+            this.status = 0;
+            this.judge();
             this.dotChange();
             this.animation(this.box, {
                 'left': -this.index * parseInt(this.w) + 'px'
             }, this.options.tTime, function() {
-                this.status = 1;
+                self.status = 1;
             }, 'px');
         }
 
@@ -340,7 +320,47 @@ window.onload = function() {
             }
         }
 
-        // 默认值
+
+        // 垂直方向平移
+        function VerticalMove(el, arr, op) {
+            Banner.call(this, el, arr, op);
+        }
+        extend(Banner, VerticalMove);
+
+        // 设置单独样式
+        VerticalMove.prototype.setStyles = function() {
+            css(this.box, {
+                'width': '100%',
+                'hight': parseInt(this.h) * this.arr.length * 3 + 'px',
+                'top': -this.index * parseInt(this.h) + 'px'
+            });
+            return this;
+        }
+
+        // 垂直平移方向的动画效果
+        VerticalMove.prototype.offset = function() {
+            var self = this;
+            this.status = 0;
+            this.judge();
+            this.dotChange();
+            this.animation(this.box, {
+                'top': -this.index * parseInt(this.h) + 'px'
+            }, this.options.tTime, function() {
+                self.status = 1;
+            }, 'px');
+        }
+
+        //垂直平移效果的判断
+        VerticalMove.prototype.judge = function() {
+                if (this.index >= this.arr.length * 2) {
+                    this.index = this.arr.length;
+                    css(this.box, 'top', -(this.index - 1) * parseInt(this.h) + 'px');
+                } else if (this.index <= this.arr.length - 1) {
+                    this.index = this.arr.length * 2 - 1;
+                    css(this.box, 'top', -(this.index + 1) * parseInt(this.h) + 'px');
+                }
+            }
+            // 默认值
         var defaultO = {
             vertical: false,
             way: 'move',
@@ -349,7 +369,8 @@ window.onload = function() {
             wTime: 2000,
             arrow: true,
             dots: true,
-            event: 'click'
+            event: 'click',
+            autoPlay: true
         };
 
         // 初始化
@@ -358,18 +379,30 @@ window.onload = function() {
             var wrap = selectEl(options.el)[0];
             var el = wrap.getElementsByTagName('*');
             var arr = [];
+            var banner;
             for (var i = 0; i < el.length; i++) {
                 if (el[i].parentNode == wrap) {
                     arr.push(el[i]);
                 }
             }
-            var banner = new Banner(wrap, arr, op);
-            banner.structure().offset().bindEvent();
+            if (op.way == 'move') {
+                if (op.vertical) {
+                    banner = new VerticalMove(wrap, arr, op);
+                } else {
+                    banner = new LevelMove(wrap, arr, op);
+                }
+            }
+            banner.structure().setStyles().bindEvent();
+            if (op.autoPlay) {
+                banner.setTimer();
+            }
             console.log(banner);
         };
         return init;
     }());
     swiper({
-        el: '.wrap'
+        el: '.wrap',
+        autoPlay: false
+            // vertical: true
     })
 }
